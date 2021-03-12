56b2a3bc06404d341367b6d69e660a3c5745b3bd
ruichaoma <ruichaoma>
2021-03-12 21:17:16 +0800

    update

--- common/src/main/java/com/kms/common/storage/init.kt
+++ common/src/main/java/com/kms/common/storage/init.kt
@@ -1,9 +1,6 @@
 package com.kms.common.storage
 
 import cn.leancloud.AVObject
-import com.kms.common.storage.Comment
-import com.kms.common.storage.Post
-import com.kms.common.storage.PostReaction
 
 fun initializeModels() {
 
